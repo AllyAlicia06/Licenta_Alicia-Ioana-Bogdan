c04e8204660aad73ead3e467a709b378ae790872
AllyAlicia06 <darkstar.ally@gmail.com>
2026-02-20 11:22:39 +0200

    Refined Combat
    -changed the way the combat feels to make it more fast paced
    -added combos for light attack
    -fixed some visual bugs when jumping and attacking to feel more nicely

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,13 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using StarterAssets;
+
 public class PlayerControl : MonoBehaviour
 {
     [Space]
     [Header("Components")]
     [SerializeField] private Animator anim;
     [SerializeField] private ThirdPersonController thirdPersonController;
-   // [SerializeField] private GameControl gameControl;
  
     [Space]
     [Header("Combat")]
@@ -22,11 +22,28 @@ public class PlayerControl : MonoBehaviour
     [SerializeField] private float attackRange = 1f;
     [SerializeField] private float reachTime = 0.3f;
     [SerializeField] private LayerMask enemyLayer;
-    bool isAttacking = false;
+    [SerializeField] private float comboResetTime = 0.5f;
+    [SerializeField] private float comboGraceTime = 0.2f;
 
+    bool isAttacking = false;
+    private int lightCombo = 0;
+    private float lastAttackEndTime = -999f; //for grace window so it can track when the last attack ended
+    private bool canCombo = false;
+    private bool bufferedLight = false;
+    private bool comboWindowOpenedThisStep = false;
+
+    private Coroutine attackTimeoutCoroutine;
+    
+    private static readonly int HashLightAttack = Animator.StringToHash("LightAttack");
+    private static readonly int HashLightCombo = Animator.StringToHash("LightCombo");
+    private static readonly int HashCanCombo = Animator.StringToHash("CanCombo");
+    
     [Space]
     [Header("Debug")]
     [SerializeField] private bool debug;
+    
+    private EnemyBase oldTarget;
+    private EnemyBase currentTarget;
 
     // Start is called before the first frame update
     void Start()
@@ -42,192 +59,158 @@ public class PlayerControl : MonoBehaviour
 
     private void FixedUpdate()
     {
-        if(target == null)
-        {
-            return;
-        }
+        if(target == null) return;
 
         if((Vector3.Distance(transform.position, target.position) >= TargetDetectionControl.instance.detectionRange))
-        {
             NoTarget();
-        }
     }
 
     void HandleInput()
     {
-        if (Input.GetMouseButtonDown(0))
-        {
+        if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.J))
             Attack(0);
-        }
 
-        if (Input.GetMouseButtonDown(1))
-        {
+        if (Input.GetMouseButtonDown(1) || Input.GetKeyDown(KeyCode.K))
             Attack(1);
-        }
-
-        if (Input.GetKeyDown(KeyCode.J))
-        {
-            Attack(0);
-        }
-
-        if (Input.GetKeyDown(KeyCode.K))
-        {
-            Attack(1);
-        }
-
     }
-
-    #region Attack, PerformAttack, Reset Attack, Change Target
-  
-
+    
     public void Attack(int attackState)
     {
         if (isAttacking)
         {
+            if (attackState == 0 && (canCombo || !comboWindowOpenedThisStep))
+                bufferedLight = true;
+            return;
+        }
+        
+        if (attackState != 0)
+        {
+            HeavyAttack();
+            return;
+        }
+
+        StartLightComboStep();
+    }
+
+    private void StartLightComboStep()
+    {
+        bufferedLight = false;
+        comboWindowOpenedThisStep = false;
+        
+        if (target == null)
+        {
+            thirdPersonController.canMove = true;
+            TargetDetectionControl.instance.canChangeTarget = true;
             return;
         }
 
         thirdPersonController.canMove = false;
         TargetDetectionControl.instance.canChangeTarget = false;
-        RandomAttackAnim(attackState);
-       
-    }
-
-    private void RandomAttackAnim(int attackState)
-    {
         
+        bool inGraceWindow = Time.time - lastAttackEndTime <= comboGraceTime;
+        bool comboExpired = Time.time - lastAttackEndTime > comboResetTime;
+        
+        if(!inGraceWindow && comboExpired && !canCombo)
+            lightCombo = 0;
 
-        switch (attackState) 
-        {
-            case 0: //Quick Attack
+        lightCombo++;
+        if (lightCombo > 3) lightCombo = 1;
+        
+        if (debug) Debug.Log($"StartLightComboStep | combo={lightCombo} | inGrace={inGraceWindow} | timeSinceEnd={Time.time - lastAttackEndTime:F2}");
+        
+        anim.SetInteger(HashLightCombo, lightCombo);
+        MoveTowardsTarget(target.position, quickAttackDeltaDistance);
+        anim.ResetTrigger(HashLightAttack);
+        anim.SetTrigger(HashLightAttack);
 
-                QuickAttack();
-                break;
-
-            case 1:
-                HeavyAttack();
-                break;
-
-        }
-
-
-       
+        isAttacking = true;
+        
+        if (attackTimeoutCoroutine != null) StopCoroutine(attackTimeoutCoroutine);
+        attackTimeoutCoroutine = StartCoroutine(AttackTimeout(2f));
     }
 
-    void QuickAttack()
+    private IEnumerator AttackTimeout(float timeout)
     {
-        int attackIndex = Random.Range(1, 4);
-        if (debug)
+        yield return new WaitForSeconds(timeout);
+        if (isAttacking)
         {
-            Debug.Log(attackIndex + " attack index");
-        }
-
-        switch (attackIndex)
-        {
-            case 1: //punch
-
-                if (target != null)
-                {
-                    MoveTowardsTarget(target.position, quickAttackDeltaDistance, "punch");
-                    isAttacking = true;
-                }
-                else
-                {
-                    thirdPersonController.canMove = true;
-                    TargetDetectionControl.instance.canChangeTarget = true;
-                }
-
-                break;
-
-            case 2: //kick
-
-                if (target != null)
-                {
-                    MoveTowardsTarget(target.position, quickAttackDeltaDistance, "kick");
-                    isAttacking = true;
-                }
-                else
-                {
-                    thirdPersonController.canMove = true;
-                    TargetDetectionControl.instance.canChangeTarget = true;
-                }
-                   
-
-                break;
-
-            case 3: //mmakick
-
-                if (target != null)
-                {
-                    MoveTowardsTarget(target.position, quickAttackDeltaDistance, "mmakick");
-
-                    isAttacking = true;
-                }
-                else
-                {
-                    thirdPersonController.canMove = true;
-                    TargetDetectionControl.instance.canChangeTarget = true;
-                }
-               
-
-                break;
+            if(debug) Debug.Log("AttackTimeout fired - player was stuck");
+            OnAttackEnd();
         }
     }
-
+    
     void HeavyAttack()
     {
-        int attackIndex = Random.Range(1, 3);
-        //int attackIndex = 2;
-        if (debug)
+        if (target == null)
         {
-            Debug.Log(attackIndex + " attack index");
+            thirdPersonController.canMove = true;
+            TargetDetectionControl.instance.canChangeTarget = true;
+            return;
         }
+        
+        int attackIndex = Random.Range(1, 3);
+        if(debug) Debug.Log(attackIndex + " heavy attack index");
+        
+        FaceThis(target.position);
+        anim.SetBool(attackIndex == 1 ? "heavyAttack1" : "heavyAttack2", true);
+        isAttacking = true;
+        
+        if (attackTimeoutCoroutine != null) StopCoroutine(attackTimeoutCoroutine);
+        attackTimeoutCoroutine = StartCoroutine(AttackTimeout(2f));
+    }
 
-        switch (attackIndex)
+    public void OpenComboWindow() //Animation event
+    {
+        canCombo = true;
+        comboWindowOpenedThisStep = true;
+        anim.SetBool(HashCanCombo, true);
+
+        if (bufferedLight && target != null && lightCombo < 3)
         {
-            case 1: //heavyAttack1
-
-                if (target != null)
-                {
-                    //MoveTowardsTarget(target.position, kickDeltaDistance, "heavyAttack1");
-                    FaceThis(target.position);
-                    anim.SetBool("heavyAttack1", true);
-                    isAttacking = true;
-                  
-                }
-                else
-                {
-                    TargetDetectionControl.instance.canChangeTarget = true;
-                    thirdPersonController.canMove = true;
-                }
-
-
-                break;
-
-            case 2: //heavyAttack2
-
-                if (target != null)
-                {
-                    //MoveTowardsTarget(target.position, kickDeltaDistance, "heavyAttack2");
-                    FaceThis(target.position);
-                    anim.SetBool("heavyAttack2", true);
-                    isAttacking = true;
-                }
-                else
-                {
-                    thirdPersonController.canMove = true;
-                    TargetDetectionControl.instance.canChangeTarget = true;
-                }
-
-                break;
+            bufferedLight = false;
+            StartLightComboStep();
         }
     }
 
-    public void ResetAttack() // Animation Event ---- for Reset Attack
+    public void CloseComboWindow() //Animation event
     {
-        anim.SetBool("punch", false);
-        anim.SetBool("kick", false);
-        anim.SetBool("mmakick", false);
+        canCombo = false;
+        anim.SetBool(HashCanCombo, false);
+        bufferedLight = false; //for preventing delayed clicks
+    }
+    
+    public void OnAttackEnd() //Animation event
+    {
+        if (!isAttacking) return;
+
+        if (attackTimeoutCoroutine != null)
+        {
+            StopCoroutine(attackTimeoutCoroutine);
+            attackTimeoutCoroutine = null;
+        }
+        
+        bufferedLight = false;
+        canCombo = false;
+        anim.SetBool(HashCanCombo, false);
+
+        isAttacking = false;
+        thirdPersonController.canMove = true;
+        TargetDetectionControl.instance.canChangeTarget = true;
+        
+        comboWindowOpenedThisStep = false;
+        lastAttackEndTime =  Time.time;
+        
+        if(debug) Debug.Log($"OnAttackEnd | combo={lightCombo} | time={Time.time: F2}");
+    }
+    
+    public void ResetAttack() //Animation event
+    {
+        if (attackTimeoutCoroutine != null)
+        {
+            StopCoroutine(attackTimeoutCoroutine);
+            attackTimeoutCoroutine =  null;
+        }
+        
         anim.SetBool("heavyAttack1", false);
         anim.SetBool("heavyAttack2", false);
         thirdPersonController.canMove = true;
@@ -235,10 +218,8 @@ public class PlayerControl : MonoBehaviour
         isAttacking = false;
     }
 
-    public void PerformAttack() // Animation Event ---- for Attacking Targets
+    public void PerformAttack() //Animation event
     {
-        // Assuming we have a melee attack with a short range
-       
         Collider[] hitEnemies = Physics.OverlapSphere(attackPos.position, attackRange, enemyLayer);
 
         foreach (Collider enemy in hitEnemies)
@@ -247,34 +228,25 @@ public class PlayerControl : MonoBehaviour
             EnemyBase enemyBase = enemy.GetComponent<EnemyBase>();
             if (enemyRb != null)
             {
-                // Calculate knockback direction
                 Vector3 knockbackDirection = enemy.transform.position - transform.position;
-                knockbackDirection.y = airknockbackForce; // Keep the knockback horizontal
-
-                // Apply force to the enemy
-                enemyRb.AddForce(knockbackDirection.normalized * knockbackForce, ForceMode.Impulse);
+                knockbackDirection.y = airknockbackForce;
+                //enemyRb.AddForce(knockbackDirection.normalized * knockbackForce, ForceMode.Impulse);
+                //this above is for the knockback of the targets, I might need to tune it down in the future or completely remove it
                 enemyBase.SpawnHitVfx(enemyBase.transform.position);
             }
         }
     }
-
-    private EnemyBase oldTarget;
-    private EnemyBase currentTarget;
+    
     public void ChangeTarget(Transform target_)
     {
         
         if(target != null)
-        {
-            //oldTarget = target_.GetComponent<EnemyBase>(); //clear old target
             oldTarget.ActiveTarget(false);
-        }
-       
+        
         target = target_;
-
-        oldTarget = target_.GetComponent<EnemyBase>(); //set current target
+        oldTarget = target_.GetComponent<EnemyBase>();
         currentTarget = target_.GetComponent<EnemyBase>();
         currentTarget.ActiveTarget(true);
-
     }
 
     private void NoTarget() // When player gets out of range of current Target
@@ -284,64 +256,40 @@ public class PlayerControl : MonoBehaviour
         oldTarget = null;
         target = null;
     }
-
-    #endregion
-
-
-    #region MoveTowards, Target Offset and FaceThis
-    public void MoveTowardsTarget(Vector3 target_, float deltaDistance, string animationName_)
+    
+    public void MoveTowardsTarget(Vector3 target_, float deltaDistance)
     {
-
-        PerformAttackAnimation(animationName_);
         FaceThis(target_);
         Vector3 finalPos = TargetOffset(target_, deltaDistance);
-        finalPos.y = 0;
+        finalPos.y = transform.position.y;
         transform.DOMove(finalPos, reachTime);
-
     }
 
-    public void GetClose() // Animation Event ---- for Moving Close to Target
+    public void GetClose() //Animation event
     {
-        Vector3 getCloseTarget;
-        if (target == null)
-        {
-            getCloseTarget = oldTarget.transform.position;
-        }
-        else
-        {
-            getCloseTarget = target.position;
-        }
+        Vector3 getCloseTarget = target != null ? target.position : oldTarget.transform.position;
         FaceThis(getCloseTarget);
         Vector3 finalPos = TargetOffset(getCloseTarget, 1.4f);
         finalPos.y = 0;
         transform.DOMove(finalPos, 0.2f);
     }
 
-    void PerformAttackAnimation(string animationName_)
-    {
-        anim.SetBool(animationName_, true);
-    }
-
     public Vector3 TargetOffset(Vector3 target, float deltaDistance)
     {
-        Vector3 position;
-        position = target;
-        return Vector3.MoveTowards(position, transform.position, deltaDistance);
+        return Vector3.MoveTowards(target, transform.position, deltaDistance);
     }
 
     public void FaceThis(Vector3 target)
     {
-        Vector3 target_ = new Vector3(target.x, target.y, target.z);
-        Quaternion lookAtRotation = Quaternion.LookRotation(target_ - transform.position);
+        Quaternion lookAtRotation = Quaternion.LookRotation(target - transform.position);
         lookAtRotation.x = 0;
         lookAtRotation.z = 0;
         transform.DOLocalRotateQuaternion(lookAtRotation, 0.2f);
     }
-    #endregion
 
     void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(attackPos.position, attackRange); // Visualize the attack range
+        Gizmos.DrawWireSphere(attackPos.position, attackRange);
     }
 }
